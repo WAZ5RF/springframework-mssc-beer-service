405a0e2e4dfdef4ac4cf2fa25f2e60c9277b6fcb
surankam <ankamsurendra.99@gmail.com>
2021-11-21 14:42:55 +0530

    Project creation. closes #1

---
+++ src/test/java/com/springframework/springframeworkmsscbeerservice/SpringframeworkMsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.springframeworkmsscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkMsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
